3d46b28f728ad4bacdec89202010c4f0f234c6f6
Daniil-Solo <daniil.solo1723@gmail.com>
2023-05-04 00:08:04 +0500

    add bem for styles

--- src/components/mobile/MobileForm.tsx
+++ src/components/mobile/MobileForm.tsx
@@ -4,44 +4,44 @@ import Button from '../UI/Button/Button'
 
 const MobileForm: React.FC = () => {
     return (
-        <>
-            <div className='mobile-form-header'>
+        <div className='mobile-form'>
+            <div className='mobile-form__header'>
                 <div className='message-container'>
-                    <p className='user-message message'>
+                    <p className='message-container__text message-container__text_user'>
                         Какой электив выбрать?
                     </p>
                 </div>
                 <div className='message-container'>
-                    <p className='bot-message message'>
+                    <p className='message-container__text message-container__text_bot'>
                         Сейчас подскажу :)
                     </p>
                 </div>
             </div>
-            <div className='mobile-form-selects'>
+            <div className='mobile-form__main'>
                 <div className="select-block">
-                    <p className='select-title'>
+                    <p className='select-block__title'>
                         Твой факультет?
                     </p>
-                    <select className='select'>
+                    <select className='select-block__select'>
                     </select>
                 </div>
                 <div className="select-block">
-                    <p className='select-title'>
+                    <p className='select-block__title'>
                         Твое направление?
                     </p>
-                    <select className='select'>
+                    <select className='select-block__select'>
                     </select>
                 </div>
                 <div className="select-block">
-                    <p className='select-title'>
+                    <p className='select-block__title'>
                         Интересуемая профессия?
                     </p>
-                    <select className='select'>
+                    <select className='select-block__select'>
                     </select>
                 </div>
                 <Button title='Готово' handleClick={(e) => e.preventDefault()}/>
             </div>
-        </>
+        </div>
         
     )
 }

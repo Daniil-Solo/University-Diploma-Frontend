0aad5442adb2e55b7c9a642e408cc5ca01280373
Daniil-Solo <daniil.solo1723@gmail.com>
2023-05-08 14:31:54 +0500

    add ElectiveGroupView

---
+++ src/components/mobile/ElectiveGroupView.tsx
@@ -0,0 +1,26 @@
+import { ElectiveGroupProps } from "../../types/electives"
+
+const ElectiveGroupView: React.FC<ElectiveGroupProps> = (props) => {
+    const {electiveGroup} = props
+    return (
+        <div className="elective-groups__item elective-group">
+            <p className="elective-group__name">
+                {electiveGroup.name + ":"}
+            </p>
+            <ul className="elective-group__items electives">
+                {
+                    electiveGroup.items.map(item => {
+                        return (
+                            <li className="electives__item" key={item.id}>
+                                {item.title}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+
+}
+
+export default ElectiveGroupView
